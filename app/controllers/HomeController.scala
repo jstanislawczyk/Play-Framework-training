ce43fdccf8d08dd4495acd3fc2e7858abe6ad5e0
xenonso <jakubstanislawczyk98@gmail.com>
2019-01-31 15:42:49 +0100

    Params, path variables, default and fixed vars

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,13 +1,28 @@
 package controllers
 
 import javax.inject._
-import play.api._
 import play.api.mvc._
 
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def index() = Action { implicit request: Request[AnyContent] =>
+  def index(): Action[AnyContent] = Action {
     Ok("Hello world")
+  }
+
+  def pathVariable(test: String): Action[AnyContent] = Action {
+    Ok(s"Some variable: $test")
+  }
+
+  def params(someString: String, someInt: Int): Action[AnyContent] = Action {
+    Ok(s"Some string: $someString and some int: $someInt")
+  }
+
+  def default(someString: String, someInt: Int): Action[AnyContent] = Action {
+    Ok(s"Default values for string: $someString and int: $someInt")
+  }
+
+  def fixed(fixedString: String): Action[AnyContent] = Action {
+    Ok(s"Fixed param: $fixedString")
   }
 }
